8433b36e51df7fcd673689b6c09919728dd4f9ae
marco741 <marco741147@gmail.com>
2019-05-24 01:40:05 +0200

    one button falling edge analog controller

---
+++ one_falling_edge_controller/one_falling_edge_controller.ino
@@ -0,0 +1,24 @@
+#include <MIDI_Controller.h>
+    
+int nuovo=0, old=0, toggle=0; 
+
+int filtro(int ingresso){
+  nuovo=ingresso/1023;
+  if(nuovo<old){
+    if(toggle==1023)
+      toggle=0;
+    else
+      toggle=1023;}
+    old=nuovo;
+    return toggle;
+}
+
+Analog button0(A0, 9, 1);
+
+void setup() {
+  button0.map(filtro);
+  }
+
+void loop() {
+  MIDI_Controller.refresh();
+}
